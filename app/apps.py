10681fcd2bff24c2ed49d38364bbb43a843a6c1c
Diksha121310 <dikshasonawane121310@gmail.com>
2025-08-09 02:28:06 +0530

    Update apps.py

--- app/apps.py
+++ app/apps.py
@@ -1,13 +1,5 @@
-<<<<<<< HEAD
 from django.apps import AppConfig
 
 
 class AppConfig(AppConfig):
     name = 'app'
-=======
-from django.apps import AppConfig
-
-
-class AppConfig(AppConfig):
-    name = 'app'
->>>>>>> 1216e640933198b15d9e1c9c5091f19f01f2dd8d

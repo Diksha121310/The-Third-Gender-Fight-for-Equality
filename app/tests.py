4d289802ff9a41cb6d1a8454f3ac170426ea0c10
Diksha121310 <dikshasonawane121310@gmail.com>
2025-08-09 02:30:00 +0530

    Update tests.py

--- app/tests.py
+++ app/tests.py
@@ -1,9 +1,4 @@
-<<<<<<< HEAD
 from django.test import TestCase
 
 # Create your tests here.
-=======
-from django.test import TestCase
 
-# Create your tests here.
->>>>>>> 1216e640933198b15d9e1c9c5091f19f01f2dd8d

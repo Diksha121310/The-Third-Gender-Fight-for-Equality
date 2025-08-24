52c57ae0e2869a035a7d3f96320d918cf5cf2517
Diksha121310 <dikshasonawane121310@gmail.com>
2025-08-25 00:10:51 +0530

    initial commit
    B

--- app/admin.py
+++ app/admin.py
@@ -1,3 +1,7 @@
+<<<<<<< HEAD
+=======
+
+>>>>>>> 4318c56 (initial commit)
 email = 'dikshasonawane1212@gmail.com'
 password = '1235'
 
--- app/apps.py
+++ app/apps.py
@@ -1,5 +1,14 @@
+<<<<<<< HEAD
+=======
+
+>>>>>>> 4318c56 (initial commit)
 from django.apps import AppConfig
 
 
 class AppConfig(AppConfig):
     name = 'app'
+<<<<<<< HEAD
+=======
+
+
+>>>>>>> 4318c56 (initial commit)

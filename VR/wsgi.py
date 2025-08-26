8aa6d8b13057a518d54b0f2ac72176974c5a347d
Diksha121310 <dikshasonawane121310@gmail.com>
2025-08-26 22:04:50 +0530

    Static files + database fix

--- VR/wsgi.py
+++ VR/wsgi.py
@@ -7,6 +7,14 @@ It exposes the WSGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
 """
+import sys, traceback
+from django.core.wsgi import get_wsgi_application
+
+try:
+    application = get_wsgi_application()
+except Exception:
+    traceback.print_exc(file=sys.stdout)
+    raise
 
 import os
 
